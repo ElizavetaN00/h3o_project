3551d165e7c5b181859dfa0dea377b2d2a96147f
Elizaveta <nikolaevaliz00@m1nt.tech>
2025-05-07 00:19:29 +0300

    Locators/User reg

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -1,6 +1,6 @@
 from data_test.creds import AddUserCreds
 from data_test.env import Env
-from data_test.locators import StartPageLocators, AddUserLocators, ContactListLocators
+from data_test.locators import StartPageLocators, AddUserLocators, ContactListPageLocators
 from pages.base_page import BasePage
 
 
@@ -10,14 +10,14 @@ def test_valid_credentials(driver):
 
     valcreds = BasePage(driver, Env.url)
     valcreds.open()
-    valcreds.click_button(StartPageLocators.sing_up_button)
+    valcreds.click_button(StartPageLocators.sign_up_button)
     valcreds.send_text(AddUserLocators.first_name, AddUserCreds.first_name)
     valcreds.send_text(AddUserLocators.last_name, AddUserCreds.last_name)
     valcreds.send_text(AddUserLocators.email, AddUserCreds.email)
     valcreds.send_text(AddUserLocators.password, AddUserCreds.password)
     valcreds.click_button(AddUserLocators.submit_button)
 
-    headers_elements = valcreds.waiting_for_all_elements(ContactListLocators.headers)
+    headers_elements = valcreds.waiting_for_all_elements(ContactListPageLocators.headers)
     headers = [header.text for header in headers_elements]
 
     assert valcreds.is_url_correct(Env.contact_list)
