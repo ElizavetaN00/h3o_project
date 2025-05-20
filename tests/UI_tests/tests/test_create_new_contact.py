ade7d518c5ca43ba2db3d5b8474a6a4fe0213b5a
uladek.kish <vldislav.kish@gmail.com>
2025-05-21 02:17:21 +0300

    TestContactDetails test_view_contact_details_page

--- tests/UI_tests/tests/test_create_new_contact.py
+++ tests/UI_tests/tests/test_create_new_contact.py
@@ -1,10 +1,10 @@
 import pytest
 
 from tests.UI_tests.data_test.constants import ErrorMsg
-from tests.UI_tests.data_test.creds import AliceContactCreds
+from tests.UI_tests.data_test.creds import ContactCreds
 from tests.UI_tests.data_test.env import Env
 from tests.UI_tests.data_test.locators import AddContactPageLocators
-from tests.UI_tests.pages.addContact import AddContact
+from tests.UI_tests.pages.add_contact import AddContact
 
 
 @pytest.mark.test_create_new_contact
@@ -16,8 +16,8 @@ class TestCreateNewContact:
     def test_all_fields(self, driver):
 
         afs = AddContact(driver, Env.url)
-        data = AliceContactCreds.return_contact_info
-        afs.enter_data(*data)
+        data = ContactCreds.return_alice_contact_info
+        afs.enter_new_contact()
 
         assert afs.is_url_correct(Env.contact_list_url)
         assert afs.convert_to_line(*data) in afs.return_contact_list()
@@ -29,8 +29,8 @@ class TestCreateNewContact:
     def test_mandatory_fields(self, driver):
 
         mf = AddContact(driver, Env.url)
-        data = AliceContactCreds.contact_info['first_name'], AliceContactCreds.contact_info['last_name']
-        mf.enter_data(*data)
+        data = ContactCreds.alice_contact_info['first_name'], ContactCreds.alice_contact_info['last_name']
+        mf.enter_new_contact()
 
         assert mf.is_url_correct(Env.contact_list_url)
         assert mf.convert_to_line(*data) in mf.return_contact_list()
@@ -40,7 +40,7 @@ class TestCreateNewContact:
     def test_only_first_name(self, driver):
 
         ofn = AddContact(driver, Env.url)
-        ofn.enter_data(first_name=AliceContactCreds.contact_info['first_name'])
+        ofn.enter_new_contact(first_name=ContactCreds.alice_contact_info['first_name'])
 
         assert (ofn.get_error_message
                 (AddContactPageLocators.error_message) == ErrorMsg.contact_last_name_error)
@@ -49,7 +49,7 @@ class TestCreateNewContact:
     def test_only_last_name(self, driver):
 
         oln = AddContact(driver, Env.url)
-        oln.enter_data(last_name=AliceContactCreds.contact_info['last_name'])
+        oln.enter_new_contact(last_name=ContactCreds.alice_contact_info['last_name'])
 
         assert (oln.get_error_message
                 (AddContactPageLocators.error_message) == ErrorMsg.contact_first_name_error)
@@ -59,20 +59,18 @@ class TestCreateNewContact:
     def test_max_length_fields(self, driver):
         mlf = AddContact(driver, Env.url)
         (fn, ln, email, street1, street2, city,
-         state_province, postal_code, country) = (AliceContactCreds.contact_info['first_name'] * 15,
-                                                  AliceContactCreds.contact_info['last_name'] * 15,
-                                                  AliceContactCreds.contact_info['email'] * 15,
-                                                  AliceContactCreds.contact_info['street1'] * 15,
-                                                  AliceContactCreds.contact_info['street2'] * 15,
-                                                  AliceContactCreds.contact_info['city'] * 15,
-                                                  AliceContactCreds.contact_info['state_province'] * 15,
-                                                  AliceContactCreds.contact_info['postal_code'] * 15,
-                                                  AliceContactCreds.contact_info['country'] * 15)
+         state_province, postal_code, country) = (ContactCreds.alice_contact_info['first_name'] * 15,
+                                                  ContactCreds.alice_contact_info['last_name'] * 15,
+                                                  ContactCreds.alice_contact_info['email'] * 15,
+                                                  ContactCreds.alice_contact_info['street1'] * 15,
+                                                  ContactCreds.alice_contact_info['street2'] * 15,
+                                                  ContactCreds.alice_contact_info['city'] * 15,
+                                                  ContactCreds.alice_contact_info['state_province'] * 15,
+                                                  ContactCreds.alice_contact_info['postal_code'] * 15,
+                                                  ContactCreds.alice_contact_info['country'] * 15)
 
-
-        mlf.enter_data(first_name=fn, last_name=ln, email=email, street1=street1,
-                       street2=street2, city=city, state_province=state_province,
-                       postal_code=postal_code, country=country)
+        mlf.enter_new_contact(first_name=fn, last_name=ln, email=email, street1=street1, street2=street2, city=city,
+                              state_province=state_province, postal_code=postal_code, country=country)
 
         assert (mlf.get_error_message
                 (AddContactPageLocators.error_message) == ErrorMsg.contact_max_len_error(fn, ln, street1, street2,
@@ -83,9 +81,8 @@ class TestCreateNewContact:
     def test_invalid_birthdate(self, driver):
 
         invbir = AddContact(driver, Env.url)
-        invbir.enter_data(first_name=AliceContactCreds.contact_info['first_name'],
-                          last_name=AliceContactCreds.contact_info['last_name'],
-                          birthdate='4045-99-98')
+        invbir.enter_new_contact(first_name=ContactCreds.alice_contact_info['first_name'],
+                                 last_name=ContactCreds.alice_contact_info['last_name'], birthdate='4045-99-98')
 
         assert (invbir.get_error_message
                 (AddContactPageLocators.error_message) == ErrorMsg.contact_birthdate_error)
@@ -94,33 +91,40 @@ class TestCreateNewContact:
     @pytest.mark.regression
     def test_invalid_email(self, driver):
         invema = AddContact(driver, Env.url)
-        invema.enter_data(first_name=AliceContactCreds.contact_info['first_name'],
-                          last_name=AliceContactCreds.contact_info['last_name'],
-                          email=AliceContactCreds.contact_info['email'][:5] + AliceContactCreds.contact_info['email'][6:])
+        invema.enter_new_contact(first_name=ContactCreds.alice_contact_info['first_name'],
+                                 last_name=ContactCreds.alice_contact_info['last_name'],
+                                 email=ContactCreds.alice_contact_info['email'][:5] +
+                                       ContactCreds.alice_contact_info['email'][6:])
 
         assert (invema.get_error_message
                 (AddContactPageLocators.error_message) == ErrorMsg.contact_email_error)
 
 
     @pytest.mark.aceptance
-    def test_special_symbol_in_email(self, driver):
+    def test_special_symbol(self, driver):
         specsym = AddContact(driver, Env.url)
-        specsym.enter_data(first_name='Alíce', last_name="O'Green", email='alíce.s+mít@test.com',
-                           phone='+1(650)7599755', street1='1101# Summit St', street2='244$ Aspen Hills Ct',
-                           city='<Evanston>', state_province='&WY', country='{USA}')
+        fn, ln, e, p, s1, s2, c, sp, cn = ContactCreds.special_symbol_contact_info['first_name'], ContactCreds.special_symbol_contact_info['last_name'], \
+                                    ContactCreds.special_symbol_contact_info['email'], ContactCreds.special_symbol_contact_info['phone'], \
+                            ContactCreds.special_symbol_contact_info['street1'], ContactCreds.special_symbol_contact_info['street2'], \
+                            ContactCreds.special_symbol_contact_info['city'], ContactCreds.special_symbol_contact_info['state_province'], \
+                    ContactCreds.special_symbol_contact_info['country']
 
-        assert specsym.convert_to_line(first_name='Alíce', last_name="O'Green", email='alíce.s+mít@test.com',
-                           phone='+1(650)7599755', street1='1101# Summit St', street2='244$ Aspen Hills Ct',
-                           city='<Evanston>', state_province='&WY', country='{USA}') in specsym.return_contact_list()
+        specsym.enter_new_contact(first_name=fn, last_name=ln, email=e, phone=p, street1=s1, street2=s2, city=c,
+                                  state_province=sp, country=cn)
+
+        assert specsym.convert_to_line(first_name=fn, last_name=ln, email=e, phone=p, street1=s1, street2=s2,
+                                       city=c, state_province=sp, country=cn) in specsym.return_contact_list()
 
 
     @pytest.mark.regression
-    def test_emoji_in_email(self, driver):
-        emoj = AddContact(driver, Env.url)
-        emoj.enter_data(first_name='Alice😉', last_name="🍇Green",
-                           street1='1101 Summit St😃', street2='244 Aspen Hills Ct😀',
-                           city='🤯Evanston', state_province='🍉WY', country='USA👾')
+    def test_emoji(self, driver):
+        emoji = AddContact(driver, Env.url)
+        fn, ln, s1, s2, c, sp, cn = ContactCreds.emoji_contact_info['first_name'], ContactCreds.emoji_contact_info['last_name'], \
+                                    ContactCreds.emoji_contact_info['street1'], ContactCreds.emoji_contact_info['street2'], \
+                                    ContactCreds.emoji_contact_info['city'], ContactCreds.emoji_contact_info['state_province'], \
+                                    ContactCreds.emoji_contact_info['country']
+        emoji.enter_new_contact(first_name=fn, last_name=ln, street1=s1, street2=s2, city=c, state_province=sp,
+                                country=cn)
 
-        assert emoj.convert_to_line(first_name='Alice😉', last_name="🍇Green",
-                           street1='1101 Summit St😃', street2='244 Aspen Hills Ct😀',
-                           city='🤯Evanston', state_province='🍉WY', country='USA👾') in emoj.return_contact_list()
+        assert emoji.convert_to_line(first_name=fn, last_name=ln, street1=s1, street2=s2,
+                                     city=c, state_province=sp, country=cn) in emoji.return_contact_list()
