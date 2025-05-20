6b2971b4a6cafe0846148a0facab6dab9152b5a5
uladek.kish <vldislav.kish@gmail.com>
2025-05-20 20:51:46 +0300

    class TestUserLogin marks x2

--- tests/UI_tests/tests/test_user_log.py
+++ tests/UI_tests/tests/test_user_log.py
@@ -25,37 +25,42 @@ class TestUserLogin:
     def test_empty_form(self, driver):
         e_form = BasePage(driver, Env.url)
         e_form.click_button(StartPageLocators.submit_button)
-        assert (e_form.get_error_message(StartPageLocators.error_message) == ErrorMsg.log_in_error)
+
+        assert e_form.get_error_message(StartPageLocators.error_message) == ErrorMsg.log_in_error
 
     @pytest.mark.regression
     def test_empty_email(self, driver):
         e_email = BasePage(driver, Env.url)
         e_email.log_in('', SimonUserCreds.password)
-        assert (e_email.get_error_message(StartPageLocators.error_message) == ErrorMsg.log_in_error)
+
+        assert e_email.get_error_message(StartPageLocators.error_message) == ErrorMsg.log_in_error
 
     @pytest.mark.regression
     def test_empty_password(self, driver):
         e_password = BasePage(driver, Env.url)
         e_password.log_in(StartPageLocators.email, '')
-        assert (e_password.get_error_message(StartPageLocators.error_message) == ErrorMsg.log_in_error)
+        assert (e_password.get_error_message
+                (StartPageLocators.error_message) == ErrorMsg.log_in_error)
 
     @pytest.mark.regression
     def test_not_registered_email(self, driver):
         not_reg_email = BasePage(driver, Env.url)
         not_reg_email.log_in(RegistrationUserCreds.email, RegistrationUserCreds.password)
-        assert (not_reg_email.get_error_message(StartPageLocators.error_message) == ErrorMsg.log_in_error)
+        assert (not_reg_email.get_error_message
+                (StartPageLocators.error_message) == ErrorMsg.log_in_error)
 
     @pytest.mark.regression
     def test_incorrect_password(self, driver):
         inc_pass = BasePage(driver, Env.url)
         inc_pass.log_in(SimonUserCreds.email, RegistrationUserCreds.password)
-        assert (inc_pass.get_error_message(StartPageLocators.error_message) == ErrorMsg.log_in_error)
+        assert (inc_pass.get_error_message
+                (StartPageLocators.error_message) == ErrorMsg.log_in_error)
 
     @pytest.mark.regression
     def test_no_dog_email(self, driver):
         no_dog = BasePage(driver, Env.url)
         no_dog.log_in(SimonUserCreds.email[:6] + SimonUserCreds.email[7:], SimonUserCreds.password)
-        assert (no_dog.get_error_message(StartPageLocators.error_message) == ErrorMsg.log_in_error)
+        assert no_dog.get_error_message(StartPageLocators.error_message) == ErrorMsg.log_in_error
 
     @pytest.mark.regression
     def test_spaces_in_email(self, driver):
