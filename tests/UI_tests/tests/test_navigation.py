4ceffceb8650204f8b74e2e934607c012c1f844d
uladek.kish <vldislav.kish@gmail.com>
2025-05-20 21:03:40 +0300

    class TestStartPage marks

--- tests/UI_tests/tests/test_navigation.py
+++ tests/UI_tests/tests/test_navigation.py
@@ -1,18 +1,27 @@
+import pytest
+
 from tests.UI_tests.data_test.constants import StringsPage
 from tests.UI_tests.data_test.env import Env
 from tests.UI_tests.data_test.locators import StartPageLocators
 from tests.UI_tests.pages.base_page import BasePage
 
 
-def test_open_start_page(driver):
-    osp = BasePage(driver, Env.url)
+@pytest.mark.navigation
+@pytest.mark.start_page
+class TestStartPage:
 
-    assert osp.get_title() == StringsPage.title
-    assert osp.get_visibility(StartPageLocators.title) is True
+    @pytest.mark.smoke
+    @pytest.mark.critical_path
+    @pytest.mark.aceptance
+    def test_open_start_page(self, driver):
+        osp = BasePage(driver, Env.url)
 
+        assert osp.get_title() == StringsPage.title
+        assert osp.get_visibility(StartPageLocators.title) is True
 
-def test_here_link(driver):
-    hl = BasePage(driver, Env.url)
-    hl.click_button(StartPageLocators.here_link)
+    @pytest.mark.smoke
+    def test_here_link(self, driver):
+        hl = BasePage(driver, Env.url)
+        hl.click_button(StartPageLocators.here_link)
 
-    assert hl.is_url_correct(Env.documenter_url)
+        assert hl.is_url_correct(Env.documenter_url)
